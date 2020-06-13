970b310ec7d3722e27d2a6d5a9b77a138b16d6e3
tamalesimon <tamalesp@gmail.com>
2020-06-14 02:08:27 +0300

    Setting up react with Mdb, fontawesome

---
+++ src/components/middle.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const MiddleSection = () => (
+    <div>
+        <h2>Welcome to React with Bootstrap</h2>
+    </div>
+)
+export default MiddleSection;
---
+++ src/components/navbar.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => (
+
+            <div>
+                <nav className="navbar navbar-dark bg-dark">
+                    <a href="/" className="navbar-brand">ReactSocial</a>
+                </nav>
+            </div>
+)
+
+export default NavBar
